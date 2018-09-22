04d9b4c3b093b68936b4914b73cf17b11869966f
Homefire <kjj7999@gmail.com>
2018-09-22 15:15:49 +0900

    init spring boot project

---
+++ src/main/java/com/homefire/amateras/AmaterasApplication.java
@@ -0,0 +1,12 @@
+package com.homefire.amateras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaterasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmaterasApplication.class, args);
+    }
+}
---
+++ src/test/java/com/homefire/amateras/AmaterasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.homefire.amateras;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmaterasApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
