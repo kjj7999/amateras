be8bad3ee24a804b0e75c3943508ce35d2419fdd
Homefire <kjj7999@gmail.com>
2018-09-22 15:26:52 +0900

    spring hello world

---
+++ src/main/java/com/homefire/amateras/controller/DefaultController.java
@@ -0,0 +1,13 @@
+package com.homefire.amateras.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}
